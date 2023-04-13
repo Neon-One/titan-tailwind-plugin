9c6e1d7678eca6d4e943f42bcfba36914404d580
Philip Theobald <ptheobald@neoncrm.com>
2023-04-12 19:17:29 -0500

    Removed commented theme import

--- js/titan-base.js
+++ js/titan-base.js
@@ -1,5 +1,4 @@
 const plugin = require('tailwindcss/plugin')
-// const titanTheme = require('./titan-theme')
 const titanButton = require('./titan-button.js')
 const titanFormDropdown = require('./titan-form-dropdown.js')
 const titanFormElement = require('./titan-form-element.js')
@@ -7,8 +6,7 @@ const titanHelperText = require('./titan-form-helper-text.js')
 const titanTextInput = require('./titan-form-text-input.js')
 const titanTextArea = require('./titan-form-text-area.js')
 
-module.exports = plugin(({ addBase, addComponents }) => {
-    // addBase(titanTheme)
+module.exports = plugin(({ addComponents }) => {
     addComponents(titanButton)
     addComponents(titanFormDropdown)
     addComponents(titanFormElement)
